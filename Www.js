c22a9de9901608943debb5f3eaf13c29b59712a3
eliiinyt <82181158+eliiinyt@users.noreply.github.com>
2022-06-06 17:29:29 -0500

    update Www.js

--- Www.js
+++ Www.js
@@ -1,5 +1,7 @@
+const { createCanvas, loadImage } = require('canvas')
+const Jimp = require("jimp");
+const fs = require("fs");
 let handler = async(m, { conn, args, usedPrefix }) => {
-  const { createCanvas, loadImage } = require('canvas')
 const canvas = createCanvas(200, 200)
 const ctx = canvas.getContext('2d')
 // Write "Awesome!"
@@ -13,8 +15,15 @@ ctx.beginPath()
 ctx.lineTo(50, 102)
 ctx.lineTo(50 + text.width, 102)
 ctx.stroke()
+/*const data =
+  canvas.toDataURL;
+const buffer = Buffer.from(data, "base64");
+Jimp.read(buffer, (err, res) => {
+  if (err) throw new Error(err);
+  res.quality(5).write("resized.jpg");
+});*/
 //console.log('<img src="' + canvas.toDataURL() + '" />')
-conn.sendFile(m.chat, canvas.toDataUrl(), '', '', m);
+conn.sendFile(m.chat, canvas.toDataURL(), '', '', m);
 }
 handler.command = /^(canvastest)$/i
 module.exports = handler
