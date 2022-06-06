8921a5de10c8f2f1c7e36e2a1ad48b0d1d1144f6
eliiinyt <82181158+eliiinyt@users.noreply.github.com>
2022-06-06 16:52:55 -0500

    update Www.js

--- Www.js
+++ Www.js
@@ -1,25 +1,15 @@
-const fs = require('fs')
 const { createCanvas, loadImage } = require('canvas')
-const width = 1200
-const height = 630
-const canvas = createCanvas(width, height)
-const context = canvas.getContext(‘2d’)
-context.fillStyle = ‘#000’
-context.fillRect(0, 0, width, height)
-context.font = ‘bold 70pt Menlo’
-context.textAlign = ‘center’
-context.textBaseline = ‘top’
-context.fillStyle = ‘#3574d4’
-const text = ‘Hello, World!’
-const textWidth = context.measureText(text).width
-context.fillRect(600 - textWidth / 2 - 10, 170 - 5, textWidth + 20, 120)
-context.fillStyle = ‘#fff’
-context.fillText(text, 600, 170)
-context.fillStyle = ‘#fff’
-context.font = ‘bold 30pt Menlo’
-context.fillText(‘flaviocopes.com’, 600, 530)
-loadImage(’./logo.png’).then(image => {
-context.drawImage(image, 340, 515, 70, 70)
-const buffer = canvas.toBuffer(‘image/png’)
-fs.writeFileSync(’./test.png’, buffer)
-})
+const canvas = createCanvas(200, 200)
+const ctx = canvas.getContext('2d')
+// Write "Awesome!"
+ctx.font = '30px Impact'
+ctx.rotate(0.1)
+ctx.fillText('Awesome!', 50, 100)
+// Draw line under text
+var text = ctx.measureText('Awesome!')
+ctx.strokeStyle = 'rgba(0,0,0,0.5)'
+ctx.beginPath()
+ctx.lineTo(50, 102)
+ctx.lineTo(50 + text.width, 102)
+ctx.stroke()
+console.log('<img src="' + canvas.toDataURL() + '" />')
