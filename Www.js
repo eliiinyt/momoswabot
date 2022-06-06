64ef4d21703c1f9b569545583e1dec5db0f49653
eliiinyt <82181158+eliiinyt@users.noreply.github.com>
2022-06-06 17:38:31 -0500

    update Www.js

--- Www.js
+++ Www.js
@@ -6,26 +6,17 @@ const canvas = createCanvas(200, 200)
 const ctx = canvas.getContext('2d')
 ctx.lineWidth = 5;
 // Color de línea
-ctx.strokeStyle = "#212121";
+ctx.strokeStyle = "#ffffff";
 // Color de relleno
 ctx.fillStyle = "#F4511E";
 // Dibujamos un rectángulo con lados iguales
-ctx.rect(5, 5, 100, 100);
+ctx.rect(5, 5, 200, 200);
 // Hacemos que se dibuje
 ctx.stroke();
 // Lo rellenamos
 ctx.fill();
 // Write "Awesome!"
-ctx.font = '30px Impact'
-ctx.rotate(0.1)
-ctx.fillText('Awesome!', 50, 100)
 // Draw line under text
-var text = ctx.measureText('Awesome!')
-ctx.strokeStyle = 'rgba(0,0,0,0.5)'
-ctx.beginPath()
-ctx.lineTo(50, 102)
-ctx.lineTo(50 + text.width, 102)
-ctx.stroke()
 /*const data =
   canvas.toDataURL;
 const buffer = Buffer.from(data, "base64");
