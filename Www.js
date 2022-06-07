048562b91a3d71f6ef1e631bca3290a6788a92df
eliiinyt <82181158+eliiinyt@users.noreply.github.com>
2022-06-06 22:37:45 -0500

    update Www.js

--- Www.js
+++ Www.js
@@ -4,7 +4,6 @@ const fs = require("fs");
 let handler = async(m, { conn, args, usedPrefix }) => {
 // Write "Awesome!"
 // Draw line under text
-function ejercicio1(){
  const canvas = createCanvas(300, 300)
 const ctx = canvas.getContext('2d')
 //Asigno a una variable el elemento del html que voy a usar
@@ -24,7 +23,6 @@ const ctx = canvas.getContext('2d')
   ctx.lineTo(300,150);// trazo la linea hasta este punto
   ctx.stroke();// levanto el lápiz
   ctx.closePath();// me alisto para realizar otra parte del dibujo
- }
 conn.sendFile(m.chat, canvas.toDataURL(), '', '', m);
 }
 handler.command = /^(canvastest)$/i
