f38854843a8935b4979b179ac963f7daea8edfb3
eliiinyt <82181158+eliiinyt@users.noreply.github.com>
2022-06-06 23:20:25 -0500

    update Www.js

--- Www.js
+++ Www.js
@@ -1,30 +1,16 @@
 const { createCanvas, loadImage } = require('canvas')
-const Jimp = require("jimp");
-const fs = require("fs");
-let handler = async(m, { conn, args, usedPrefix }) => {
-// Write "Awesome!"
-// Draw line under text
- const canvas = createCanvas(600, 600)
-const ctx = canvas.getContext('2d')
-ctx.fillStyle = "#ffffff";
-ctx.fillRect(0,600,600,600)
-//Asigno a una variable el elemento del html que voy a usar
- //Alisto el canvas para que funcione
-  ctx.lineWidth=1; //Defino el ancho de la linea en pixeles
-  ctx.strokeStyle = '#000'; //Defino el color en hexagesimal
- 
-//EJEX
-  ctx.beginPath(); // Pongo el lápiz
-  ctx.moveTo(150,0); // lo ubicó para iniciar el dibujo
-  ctx.lineTo(150,300); // trazo la linea hasta este punto
-  ctx.stroke(); // levanto el lápiz
-  ctx.closePath(); // me alisto para realizar otra parte del dibujo
-//EJE Y
-  ctx.beginPath(); // Pongo el lápiz
-  ctx.moveTo(0,150);// lo ubicó para iniciar el dibujo
-  ctx.lineTo(300,150);// trazo la linea hasta este punto
-  ctx.stroke();// levanto el lápiz
-  ctx.closePath();// me alisto para realizar otra parte del dibujo
+let handler = async (m) => {
+  let img1 = await fetch('https://telegra.ph/file/988bf42ae67248d8c5a9d.jpg')
+  let q = m.quoted ? m.quoted : m
+  let mime = (q.msg || q).mimetype || '';
+  if (!mime) throw '¿y la imágen?';
+  let media = await q.download();
+  let isImg = /image\/(png|jpe?g|gif)/.test(mime);
+const canvas = createCanvas(1200, 1200);
+const ctx = canvas.getContext('2d');
+var img = new Image();
+img.src = img1;
+ctx.drawImage(img, 0, 0, 200, 1200);
 conn.sendFile(m.chat, canvas.toDataURL(), '', '', m);
 }
 handler.command = /^(canvastest)$/i
