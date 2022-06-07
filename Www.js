422e50654f0e0a2d2a2dbce18f3011dc4b88f2e3
eliiinyt <82181158+eliiinyt@users.noreply.github.com>
2022-06-06 22:36:08 -0500

    update Www.js

--- Www.js
+++ Www.js
@@ -2,29 +2,29 @@ const { createCanvas, loadImage } = require('canvas')
 const Jimp = require("jimp");
 const fs = require("fs");
 let handler = async(m, { conn, args, usedPrefix }) => {
-const canvas = createCanvas(200, 200)
-const ctx = canvas.getContext('2d')
-ctx.lineWidth = 5;
-// Color de línea
-ctx.strokeStyle = "#000000";
-// Color de relleno
-ctx.fillStyle = "#ffffff";
-// Dibujamos un rectángulo con lados iguales
-ctx.rect(5, 5, 200, 200);
-// Hacemos que se dibuje
-ctx.stroke();
-// Lo rellenamos
-ctx.fill();
 // Write "Awesome!"
 // Draw line under text
-/*const data =
-  canvas.toDataURL;
-const buffer = Buffer.from(data, "base64");
-Jimp.read(buffer, (err, res) => {
-  if (err) throw new Error(err);
-  res.quality(5).write("resized.jpg");
-});*/
-//console.log('<img src="' + canvas.toDataURL() + '" />')
+function ejercicio1(){
+ const canvas = createCanvas(300, 300)
+const ctx = canvas.getContext('2d')
+//Asigno a una variable el elemento del html que voy a usar
+ //Alisto el canvas para que funcione
+  ctx.lineWidth=1; //Defino el ancho de la linea en pixeles
+  ctx.strokeStyle = '#000'; //Defino el color en hexagesimal
+ 
+//EJEX
+  ctx.beginPath(); // Pongo el lápiz
+  ctx.moveTo(150,0); // lo ubicó para iniciar el dibujo
+  ctx.lineTo(150,300); // trazo la linea hasta este punto
+  ctx.stroke(); // levanto el lápiz
+  ctx.closePath(); // me alisto para realizar otra parte del dibujo
+//EJE Y
+  ctx.beginPath(); // Pongo el lápiz
+  ctx.moveTo(0,150);// lo ubicó para iniciar el dibujo
+  ctx.lineTo(300,150);// trazo la linea hasta este punto
+  ctx.stroke();// levanto el lápiz
+  ctx.closePath();// me alisto para realizar otra parte del dibujo
+ }
 conn.sendFile(m.chat, canvas.toDataURL(), '', '', m);
 }
 handler.command = /^(canvastest)$/i
