76e0a9b470c7c9aa0fdd4092969ec80518fddd9d
eliiinyt <82181158+eliiinyt@users.noreply.github.com>
2022-06-09 05:22:56 -0500

    update Www.js

--- Www.js
+++ Www.js
@@ -1,14 +1,25 @@
-module.exports = handler
 let fetch = require('node-fetch');
-let handler = async(m, { conn, usedPrefix, command }) => {
-  let res = await fetch('https://api.waifu.pics/sfw/waifu');
+let handler = async(m, { conn, usedPrefix, command, text }) => {
+  let res = await fetch('https://api.lolhuman.xyz/api/nhentai/' + `${text}` + '?apikey=9b817532fadff8fc7cb86862');
   if (!res.ok) throw await res.text();
   let json = await res.json();
-  if (!json.url) throw 'Ups, un error!';
-  conn.send2ButtonImg(m.chat, json.url, '¿qué tal?', 'sexo', '¿Otra?', `${usedPrefix + command}`, '¿nsfw?', `${usedPrefix}nsfw`, m);
+  let infos = json.result.info;
+  console.log(infos);
+  let respuesta = `*Título:*\n${json.result.title_romaji}\n*información:*\n*personajes:* ${infos.characters}\n\n*etiquetas:* ${infos.tags}\n\n*paginas:* ${infos.pages}`;
+  let paginas = infos.pages;
+  //console.log(res);
+  //console.log(json);
+ await conn.sendButtonImg(m.chat, json.result.image[0], `${respuesta}`, 'sexo', '¿Otra?', `${bucle}`, m);
+console.log(paginas);
+  async function bucle() {
+    for (let i=0; i < paginas; i++ ) {
+    await conn.sendFile(m.chat, json.result.image[i], `${i}`, `pagina: ${i}`, m);
+    console.log('hola');
+    }
+    }
+bucle();
 };
-handler.help = ['waifu'];
+handler.help = ['nhentai <code>'];
 handler.tags = ['internet'];
-handler.command = /^(waifu)$/i;
+handler.command = /^(nhentai)$/i;
 module.exports = handler;
-
