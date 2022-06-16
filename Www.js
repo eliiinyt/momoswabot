d3c88ee89c77fb28e82081c0ee8b81fe1736fc42
eliiinyt <82181158+eliiinyt@users.noreply.github.com>
2022-06-16 01:29:08 -0500

    update Www.js

--- Www.js
+++ Www.js
@@ -3,25 +3,12 @@ let fs = require('fs')
 let fetch = require('node-fetch')
 let { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, participants, args }) => {
-let vn = './media/menu.mp3'
-const rows = [
-//LO QUE SE MUESTRA + DESCRIPCION + COMANDO 
-{title: 'waifu', description: "envía una imágen de una waifu random", rowId:".waifu"},
-{title: 'maid', description: "manda una imagen de una maid", rowId:".maid"},
-{title: 'Marin', description: "envía una imagen de Marin Kitagawa", rowId:".marin"},
-{title: 'uniforme', description: "envía una imagen de una waifu con uniforme", rowId:".uniforme"},
+  const templateButtons = [
+    {index: 1, urlButton: {displayText: '⭐ Star Baileys on GitHub!', url: 'https://github.com/adiwajshing/Baileys'}},
+    {index: 2, callButton: {displayText: 'Call me!', phoneNumber: '+1 (234) 5678-901'}},
+    {index: 3, quickReplyButton: {displayText: 'This is a reply, just like normal buttons!', id: 'id-like-buttons-message'}},
 ]
-const sections = [{title: "Comandos de internet:", rows: rows}]
-const button = {
-buttonText: 'SELECCIONE UNA OPCIÓN',
-description: "Lista de comandos de internet:\n\n_エリンボット四番_",
-sections: sections,
-listType: 1
-//NOTA: PARA HACER UN SALTO DE LINEA DE USA \n 
-//NOTA 2: EL COMANDO PARA PEDIR ESTE MENU ES #wamenu
-//NOTA 3: PARA ACTIVAR ESTE COMANDO DEBES ELIMINAR LOS /* DEL INICIO Y EL */ /* DEL FINAL DE ESTE CODIGO
-}  
-conn.sendMessage(m.chat, button, MessageType.listMessage)
+conn.sendMessage(m.chat, id, templateMessage)
 }
 handler.command = /^menuinternet$/i
 handler.fail = null
