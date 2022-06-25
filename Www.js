51c37521c392a63362144d8b08a2e9bb3ea700d5
eliiinyt <82181158+eliiinyt@users.noreply.github.com>
2022-06-25 01:32:58 -0500

    Update Www.js

--- Www.js
+++ Www.js
@@ -1,30 +1,28 @@
 [
- "¿Alguna vez has tomado el dinero de tus padres o no?",
- "¿Alguna vez le has mentido a algún padre?\nCuéntame sobre la mentira",
- "¿Que comida te gusta?",
- "¿Quién quiere ser novia en esta GC?",
- "¿Cuál es tu peor pesadilla?",
- "¿Qué es lo más vergonzoso de tu amigo?",
- "¿Alguna vez te ha gustado alguien? ¿Cuánto tiempo?",
- "Si puedes o si quieres, ¿de qué gc/gc externo te harías amigo? (tal vez diferente/del mismo tipo)",
- "¿Cuál es tu mayor miedo?",
- "¿Alguna vez te ha gustado alguien y has sentido que a esa persona también le gustas?",
- "¿Cómo se llama la ex novia de tu amigo que te gustaba en secreto?",
- "¿Alguna vez le has robado dinero a tu padre o a tu padre? ¿La razón?",
- "¿Qué te hace feliz cuando estás triste?"
- "¿Alguna vez has tenido un amor unilateral? ¿Si alguna vez has estado con quién? ¿Cómo se siente, hermano?",
- "¿Alguna vez has sido la amante de alguien?",
- "Lo más temido"
- "¿Quién es la persona más influyente en tu vida?"
- "¿De qué estás orgulloso este año?"
- "Quien te puede hacer grande :v",
- "¿Quién es la persona que te ha hecho grande alguna vez?"
- "(Para los musulmanes) ¿alguna vez no habéis orado en todo el día?",
- "¿Quién se acerca más a tu tipo de pareja ideal aquí?"
- "¿Te gusta mabar (jugar juntos) con quién?",
- "¿Alguna vez personas rechazadas? ¿Por qué?",
- "Nombra un incidente que te hizo sentir herido y que todavía recuerdas".
- "¿Qué has logrado este año?"
- "¿Cuál es tu peor hábito en la escuela?"
- "¿Cómo se llama el ex que te hace daño?"
+ "¿Eres lo suficientemente valiente como para tomar una foto de un cementerio de medianoche?",
+ "Toma una foto del bot, conviértela en tu foto de perfil durante 1 día",
+ "VN canta mi globo hay 5",
+ "Bebe Coca-Cola hasta que se acabe sin eructar durante 30 segundos",
+ "Come 1 Chili sin beber, sin freír por 2 minutos",
+ "Sumerge tu celular en agua por 30 segundos",
+ "VN *Te amo*",
+ "Corte de ganso de pato cantor (VN)",
+ "Envíale un mensaje a tu ex y dile _todavía me gustas_",
+ "Llamar a la enamorada/novia ahora y enviar un mensaje de texto al jugador",
+ "Papá a un miembro del grupo",
+ "Papá lo que tienes delante",
+ "SS llamada reciente whatsapp",
+ "Envía una nota de voz diciendo ¿puedo llamarte bebé?",
+ "Usar la foto de Sule durante 3 días",
+ "Escriba en el idioma local las 24 horas",
+ "Cambiar nombre a _gue lucinta luna_ por 5 horas",
+ "Chatea para contactar a wa en el orden de acuerdo a tu %batre, luego dile _tengo suerte de tenerte_",
+ "Habla de broma con tu ex y di *te amo, por favor vuelve*",
+ "Grabar voz leer sura al-kautsar",
+ "Cambiar nombre a *BOWO* por 24 horas",
+ "¡Nombra tu tipo de novio!",
+ "VN *Te amo*",
+ "¡Tienes que papilla ahora!",
+ "Debes dividir el límite, al menos 2 por usuario",
+ "El chat de 1 hora debe ser 4l4Y 8AN93T"
  ]
