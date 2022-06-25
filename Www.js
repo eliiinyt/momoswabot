a8d4206d1cfe6ab4fb13a07de51d0f23637a3916
eliiinyt <82181158+eliiinyt@users.noreply.github.com>
2022-06-25 01:28:41 -0500

    Update Www.js

--- Www.js
+++ Www.js
@@ -1,15 +1,30 @@
-
-let fs = require('fs')
-let fetch = require('node-fetch')
-let { MessageType } = require('@adiwajshing/baileys')
-let handler = async (m, { conn, participants, args }) => {
-  const templateButtons = [
-    {index: 1, urlButton: {displayText: '⭐ Star Baileys on GitHub!', url: 'https://github.com/adiwajshing/Baileys'}},
-    {index: 2, callButton: {displayText: 'Call me!', phoneNumber: '+1 (234) 5678-901'}},
-    {index: 3, quickReplyButton: {displayText: 'This is a reply, just like normal buttons!', id: 'id-like-buttons-message'}},
-]
-conn.sendMessage(m.chat, id, templateMessage)
-}
-handler.command = /^menuinternet$/i
-handler.fail = null
-module.exports = handler
+[
+ "¿Alguna vez has tomado el dinero de tus padres o no?",
+ "¿Alguna vez le has mentido a algún padre?\nCuéntame sobre la mentira",
+ "¿Que comida te gusta?",
+ "¿Quién quiere ser novia en esta GC?",
+ "¿Cuál es tu peor pesadilla?",
+ "¿Qué es lo más vergonzoso de tu amigo?",
+ "¿Alguna vez te ha gustado alguien? ¿Cuánto tiempo?",
+ "Si puedes o si quieres, ¿de qué gc/gc externo te harías amigo? (tal vez diferente/del mismo tipo)",
+ "¿Cuál es tu mayor miedo?",
+ "¿Alguna vez te ha gustado alguien y has sentido que a esa persona también le gustas?",
+ "¿Cómo se llama la ex novia de tu amigo que te gustaba en secreto?",
+ "¿Alguna vez le has robado dinero a tu padre o a tu padre? ¿La razón?",
+ "¿Qué te hace feliz cuando estás triste?"
+ "¿Alguna vez has tenido un amor unilateral? ¿Si alguna vez has estado con quién? ¿Cómo se siente, hermano?",
+ "¿Alguna vez has sido la amante de alguien?",
+ "Lo más temido"
+ "¿Quién es la persona más influyente en tu vida?"
+ "¿De qué estás orgulloso este año?"
+ "Quien te puede hacer grande :v",
+ "¿Quién es la persona que te ha hecho grande alguna vez?"
+ "(Para los musulmanes) ¿alguna vez no habéis orado en todo el día?",
+ "¿Quién se acerca más a tu tipo de pareja ideal aquí?"
+ "¿Te gusta mabar (jugar juntos) con quién?",
+ "¿Alguna vez personas rechazadas? ¿Por qué?",
+ "Nombra un incidente que te hizo sentir herido y que todavía recuerdas".
+ "¿Qué has logrado este año?"
+ "¿Cuál es tu peor hábito en la escuela?"
+ "¿Cómo se llama el ex que te hace daño?"
+ ]
