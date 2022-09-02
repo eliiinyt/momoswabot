c62c8f2eb2edc42f69397d2d601014b6ce3772cb
eliiinyt <82181158+eliiinyt@users.noreply.github.com>
2022-09-02 11:49:57 -0400

    update Www.js

--- Www.js
+++ Www.js
@@ -1,1 +1,18 @@
-░◢█◣◢█◣░ ░██████░ ░◥████◤░ ░░◥██◤░░ ░░░◥◤ᴛᴇ ᴘᴜᴇᴅᴏ ᴄʜᴜᴘᴀʀ ʟᴀ ᴠᴀɢɪɴᴀ? bbPensar demasiado en algo que no conozco es perder el tiempo.Las emociones son el pecado de la inteligencia
+let handler = async (m, { conn, args }) => {
+ 
+if (random(0, 6) == 1) {
+  let ownerGroup = m.chat.split`-`[0] + '@s.whatsapp.net'
+  
+  let users = m.mentionedJid.filter(u => !(u == ownerGroup || u.includes(conn.user.jid)))
+
+  for (let user of users) if (user.endsWith('@s.whatsapp.net')) await conn.groupParticipantsUpdate(m.chat, [user], "perdió la vida")
+m.reply(m.chat, 'pendejo perdió la ruleta', m)
+  
+}
+function random(min, max) {
+    return Math.floor((Math.random() * (max - min + 1)) + min);
+}
+handler.help = ['numran <Numero1 Numero2>'];
+handler.command = /^(ruleta rusa)$/i;
+handler.tags = ['main'];
+module.exports = handler
